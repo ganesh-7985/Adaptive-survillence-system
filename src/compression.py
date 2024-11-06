dd73474a0981aed03e7e5dc9d96bb08ef1128145
ganesh-7985 <shankarganesh3005@gmail.com>
2024-11-06 20:12:06 +0530

    updated code

--- src/compression.py
+++ src/compression.py
@@ -1,10 +1,19 @@
 import ffmpeg
 
+# def compress_video(input_folder, output_file):
+#     output_path = f'/Users/shankarganesh/Desktop/asc/output/{output_file}'
+#     (
+#         ffmpeg
+#         .input(f'{input_folder}/*.jpg', pattern_type='glob', framerate=30)
+#         .output(output_path, vcodec='libx265', crf=28)
+#         .run()
+#     )
+
 def compress_video(input_folder, output_file):
     output_path = f'/Users/shankarganesh/Desktop/asc/output/{output_file}'
     (
         ffmpeg
-        .input(f'{input_folder}/*.jpg', pattern_type='glob', framerate=30)
+        .input(f'{input_folder}/*.jpg', pattern_type='glob', framerate=30, analyzeduration=10000000, probesize=5000000)
         .output(output_path, vcodec='libx265', crf=28)
         .run()
     )
--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,11 @@ import cv2
 import os
 
 def save_frame(frame, frame_count, relevant_count, irrelevant_count, frame_type):
-    folder = 'relevant' if frame_type == 'relevant' else 'irrelevant'
+    folder = '/Users/shankarganesh/Desktop/asc/output/relevant' if frame_type == 'relevant' else '/Users/shankarganesh/Desktop/asc/output/irrelevant'
     filename = f"{folder}/frame_{relevant_count if frame_type == 'relevant' else irrelevant_count}.jpg"
-    cv2.imwrite(f'../output/{filename}', frame)
+    cv2.imwrite(filename, frame)
     print(f"Frame {frame_count}: {frame_type.capitalize()} - Saved to {filename}")
+
 
 
 def log_detection(log_data, frame_count, frame_type, timestamp):
